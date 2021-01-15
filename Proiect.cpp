0407356a7b89fb17e7286a350bf0b8189f99ab1f
tricaisabel <72613613+tricaisabel@users.noreply.github.com>
2021-01-15 17:56:31 +0200

    Update Proiect.cpp

--- Proiect.cpp
+++ Proiect.cpp
@@ -8,9 +8,9 @@ using namespace std;
 
 // verifica daca tabelul exista
 bool tabelul_exista(string nume_tabel, database& d) {
-	for (int i=0;i<d.tabele.size();i++)
+	for (int i = 0; i < d.tabele.size(); i++)
 	{
-		if (d.tabele[i]->afiseaza_nume_tabel() == nume_tabel) 
+		if (d.tabele[i]->getNumeT() == nume_tabel)
 		{
 			return true;
 		}
@@ -20,9 +20,9 @@ bool tabelul_exista(string nume_tabel, database& d) {
 
 
 // functie pentru prelucrarea comenzilor primite
-bool prelucrare_comanda(vector<string> comanda, database& d) {
+bool prelucrare_comanda(vector<string> comanda, database& d, bool& err) {
 	// daca nu primim nicio comanda, continuam sa citim comenzi
-	if (comanda.size() == 0) 
+	if (comanda.size() == 0)
 	{
 		return true;
 	}
@@ -38,14 +38,14 @@ bool prelucrare_comanda(vector<string> comanda, database& d) {
 		// trebuie sa avem cel putin 3 parametri
 		if (comanda.size() >= 7)
 		{
-			if (comanda[1] == "TABLE") 
+			if (comanda[1] == "TABLE")
 			{
 				// verifica daca exista tabelul
-				if (tabelul_exista(comanda[2], d)) 
+				if (tabelul_exista(comanda[2], d))
 				{
 					cout << "> Tabelul cu numele " << comanda[2] << " exista!" << endl;
 				}
-				else 
+				else
 				{
 					// inseram un nou tabel daca nu exista
 					//citim restul parametriilor
@@ -56,11 +56,13 @@ bool prelucrare_comanda(vector<string> comanda, database& d) {
 					}
 					d.Create_table(comanda[2], parametrii);
 					cout << "> Tabelul " << comanda[2] << " a fost adaugat cu succes!" << endl;
+					err = false;
 				}
 			}
 		}
 		else {
-			cout << "> Comanda incompleta!\n> UTILIZARE: CREATE TABLE <nume_tabel> nume_coloana1 tip1 dimensiune1 nume_coloana2 tip 2 etc." << endl;
+			cout << "> Comanda incompleta!\n> UTILIZARE: CREATE TABLE <nume_tabel> <nume_coloana1> <tip1> <dimensiune1> <val_implicita_1> [nume_coloana2] [tip_2] [val_implicita_2] ..." << endl;
+			err = true;
 		}
 	}
 
@@ -69,7 +71,7 @@ bool prelucrare_comanda(vector<string> comanda, database& d) {
 		// trebuie sa avem cel putin 3 parametri
 		if (comanda.size() >= 3) {
 			if (comanda[1] == "TABLE") {
-				if (tabelul_exista(comanda[2], d)) 
+				if (tabelul_exista(comanda[2], d))
 				{
 					d.Drop_table(comanda[2]);
 				}
@@ -79,17 +81,17 @@ bool prelucrare_comanda(vector<string> comanda, database& d) {
 				}
 			}
 		}
-		else 
+		else
 		{
 			cout << "> Comanda incompleta!\n> UTILIZARE: DROP TABLE <nume_tabel>" << endl;
 		}
 	}
 
 	// comanda: DISPLAY TABLE <nume_tabel>
-	else if (comanda[0] == "DISPLAY") 
+	else if (comanda[0] == "DISPLAY")
 	{
 		// trebuie sa avem cel putin 3 parametri
-		if (comanda.size() >= 3) 
+		if (comanda.size() >= 3)
 		{
 			if (comanda[1] == "TABLE")
 			{
@@ -103,29 +105,29 @@ bool prelucrare_comanda(vector<string> comanda, database& d) {
 				}
 			}
 		}
-		else 
+		else
 		{
 			cout << "> Comanda incompleta!\n> UTILIZARE: DISPLAY TABLE <nume_tabel>" << endl;
 		}
 	}
 
 	// comanda SELECT ALL FROM <nume_tabel>
-	else if (comanda[0] == "SELECT" && comanda[1] == "ALL" && comanda[2]=="FROM")
+	else if (comanda[0] == "SELECT" && comanda[1] == "ALL" && comanda[2] == "FROM")
 	{
 		if (comanda.size() >= 4)
 		{
 			if (tabelul_exista(comanda[3], d))
 			{
-				if (comanda.size() ==8)
+				if (comanda.size() == 8)
 				{
 					if (comanda[4] == "WHERE")
 					{
-						d.Select_All(comanda[3], comanda[5], comanda[7]);
+						d.Select(comanda[3], comanda[5], comanda[7]);
 					}
 				}
 				else
 				{
-					d.Select_All(comanda[3], "", "");
+					d.Select(comanda[3]);
 				}
 			}
 			else
@@ -140,37 +142,48 @@ bool prelucrare_comanda(vector<string> comanda, database& d) {
 	}
 
 	// comanda SELECT coloana_1 coloana_2 ... FROM <nume_tabel>
-	else if (comanda[0] == "SELECT" && comanda[1]!="ALL")
+	/*else if (comanda[0] == "SELECT" && comanda[1] != "ALL")
 	{
-	if (comanda.size() >= 4)
-	{
-		vector<string> coloane;
-		int pos = 1;
-		while (comanda[pos] != "FROM")
+		if (comanda.size() >= 4)
 		{
-			coloane.push_back(comanda[pos]);
-			pos++;
-		}
+			vector<string> coloane;
+			int pos = 1;
+			while (comanda[pos] != "FROM")
+			{
+				coloane.push_back(comanda[pos]);
+				pos++;
+			}
 
-		if (tabelul_exista(comanda[pos+1], d))
-		{
-			d.Select(comanda[pos+1],coloane);
+			if (tabelul_exista(comanda[pos + 1], d))
+			{
+				if (comanda.(pos+2,comanda.end()).size()==)
+				{
+					if (comanda[4] == "WHERE")
+					{
+						d.Select(comanda[3], comanda[5], comanda[7]);
+					}
+				}
+				else
+				{
+					d.Select(comanda[3]);
+				}
+				d.Select(comanda[pos + 1], coloane);
+			}
+			else
+			{
+				cout << "> Tabelul cu numele " << comanda[pos + 1] << " nu exista!" << endl;
+			}
 		}
 		else
 		{
-			cout << "> Tabelul cu numele " << comanda[pos+1] << " nu exista!" << endl;
+			cout << "> Comanda incompleta!\n> UTILIZARE: SELECT coloana_1 coloana_2 ... FROM  <nume_tabel>" << endl;
 		}
 	}
-	else
-	{
-		cout << "> Comanda incompleta!\n> UTILIZARE: SELECT coloana_1 coloana_2 ... FROM  <nume_tabel>" << endl;
-	}
-	}
-
+	*/
 	//comanda INSERT INTO <nume_tabela> VALUES(...)
 	else if (comanda[0] == "INSERT" && comanda[1] == "INTO")
 	{
-		if (comanda.size() >= 4 && comanda[3]=="VALUES")
+		if (comanda.size() >= 4 && comanda[3] == "VALUES")
 		{
 			if (tabelul_exista(comanda[2], d))
 			{
@@ -195,18 +208,15 @@ bool prelucrare_comanda(vector<string> comanda, database& d) {
 	//comanda UPDATE nume_tabela SET nume_coloana = valoare WHERE nume_coloana = valoare
 	else if (comanda[0] == "UPDATE")
 	{
-		if (comanda.size() == 10 && comanda[2]=="SET" && comanda[6]=="WHERE" && comanda[4]=="=" && comanda[8]=="=" )
+		if (comanda.size() == 10 && comanda[2] == "SET" && comanda[6] == "WHERE" && comanda[4] == "=" && comanda[8] == "=")
 		{
 			string nume_tabel = comanda[1];
 			string coloana_where = comanda[7];
 			string valoare_where = comanda[9];
 			string coloana_set = comanda[3];
 			string valoare_set = comanda[5];
-			bool ok=d.Update(nume_tabel, coloana_where, valoare_where, coloana_set, valoare_set);
-			if (ok == true)
-			{
-				cout << "\n> Valoarea a fost modificata" << endl;
-			}
+			d.Update(nume_tabel, coloana_where, valoare_where, coloana_set, valoare_set);
+			cout << "\n> Valoarea a fost modificata" << endl;
 		}
 		else
 		{
@@ -214,7 +224,7 @@ bool prelucrare_comanda(vector<string> comanda, database& d) {
 		}
 	}
 
-	else 
+	else
 	{
 		cout << "> Comanda incorecta!" << endl;
 	}
@@ -222,23 +232,39 @@ bool prelucrare_comanda(vector<string> comanda, database& d) {
 	return true;
 }
 
-void citire_comenzi() 
+void afisare_meniu()
+{
+	cout << "Optiuni posibile:\n";
+	cout << "\tCREATE TABLE <nume_tabel> <nume_coloana1> <tip1> <dimensiune1> <val_implicita_1> [nume_coloana2] [tip_2] [val_implicita_2] ...\n";
+	cout << "\tDISPLAY TABLE <nume_tabel>\n";
+	cout << "\tSELECT ALL FROM <nume_tabel>\n";
+	cout << "\tSELECT coloana_1 [coloana_2] ... FROM <nume_tabel>\n";
+	cout << "\tINSERT INTO <nume_tabela> VALUES <value_1> <value_2> ...\n";
+	cout << "\tUPDATE <nume_tabela> SET <nume_coloana> = <valoare> WHERE <nume_coloana> = <valoare>\n";
+	cout << "\tDROP TABLE <nume_tabel>\n";
+}
+
+void citire_comenzi()
 {
 	// cat timp se citeste de la tastatura
-	bool seCiteste = true;
+	bool seCiteste = true, err = false;
 	database d;
 
 	while (seCiteste)
 	{
+		if (err == false) afisare_meniu();
 		string linie;
 		cout << "\nIntroduceti comanda:" << endl;
 		getline(cin, linie);
 		istringstream iss(linie);
 		// imparte linia in vector de cuvinte
 		vector<string> comanda((istream_iterator<string>(iss)), istream_iterator<string>());
-		seCiteste = prelucrare_comanda(comanda, d);
+		seCiteste = prelucrare_comanda(comanda, d, err);
 	}
 }
+//de implementat delete folosind metoda Delete(string nume_tabel, string coloana_where, string valoare_where)
+//de facut sa mearga select coloane from tabel where coloana = valoare
+//de adaugat noi functii
 
 void main()
 {
